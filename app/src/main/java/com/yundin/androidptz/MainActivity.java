c9e43a805caad74d0fb108f52b40a689df9dc553
Vladislav Yundin <4yundin@gmail.com>
2019-02-19 18:14:05 +0300

    Подключил onvif как в туториале

--- app/src/main/java/com/yundin/androidptz/MainActivity.java
+++ app/src/main/java/com/yundin/androidptz/MainActivity.java
@@ -1,14 +1,49 @@
 package com.yundin.androidptz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.rvirin.onvif.onvifcamera.OnvifDevice;
+import com.rvirin.onvif.onvifcamera.OnvifListener;
+import com.rvirin.onvif.onvifcamera.OnvifRequest;
+import com.rvirin.onvif.onvifcamera.OnvifResponse;
+
+import static com.rvirin.onvif.onvifcamera.OnvifDeviceKt.currentDevice;
+
+public class MainActivity extends AppCompatActivity implements OnvifListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentDevice = new OnvifDevice("IP_ADDRESS:PORT", "login", "pwd");
+        currentDevice.setListener(this);
+        currentDevice.getServices();
+    }
+
+    @Override
+    public void requestPerformed(@NonNull OnvifResponse onvifResponse) {
+        String request = "Request " + onvifResponse.getRequest().getType() + " performed.";
+        Log.d("ONVIF", request);
+        String succeeded = "Succeeded: " + onvifResponse.getSuccess() + ", message: " + onvifResponse.getParsingUIMessage();
+        Log.d("ONVIF",succeeded);
+
+        if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetServices) {
+
+            currentDevice.getDeviceInformation();
+        } else if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetDeviceInformation) {
+
+            currentDevice.getProfiles();
+        } else if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetProfiles) {
+
+            currentDevice.getStreamURI();
+        } else if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetStreamURI) {
+
+            Log.d("ONVIF", "Stream URI retrieved: " + currentDevice.getRtspURI());
+        }
     }
 }
