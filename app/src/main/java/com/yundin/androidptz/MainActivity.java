be9dc0fa094ff848600e2a6d35ea72ee4c15eef7
Vladislav Yundin <4yundin@gmail.com>
2019-02-19 22:49:13 +0300

    Добавлен ContinuousMove, кнопки, вырезано все остальное

--- app/src/main/java/com/yundin/androidptz/MainActivity.java
+++ app/src/main/java/com/yundin/androidptz/MainActivity.java
@@ -1,49 +1,119 @@
 package com.yundin.androidptz;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
-import com.rvirin.onvif.onvifcamera.OnvifDevice;
-import com.rvirin.onvif.onvifcamera.OnvifListener;
-import com.rvirin.onvif.onvifcamera.OnvifRequest;
-import com.rvirin.onvif.onvifcamera.OnvifResponse;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-import static com.rvirin.onvif.onvifcamera.OnvifDeviceKt.currentDevice;
-
-public class MainActivity extends AppCompatActivity implements OnvifListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        currentDevice = new OnvifDevice("192.168.15.43:80", "student", "student");
-        currentDevice.setListener(this);
-        currentDevice.getServices();
+        findViewById(R.id.up).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Action().execute("up");
+            }
+        });
+
+        findViewById(R.id.down).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Action().execute("down");
+            }
+        });
+
+        findViewById(R.id.left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Action().execute("left");
+            }
+        });
+
+        findViewById(R.id.right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Action().execute("right");
+            }
+        });
     }
 
-    @Override
-    public void requestPerformed(@NonNull OnvifResponse onvifResponse) {
-        String request = "Request " + onvifResponse.getRequest().getType() + " performed.";
-        Log.d("ONVIF", request);
-        String succeeded = "Succeeded: " + onvifResponse.getSuccess() + ", message: " + onvifResponse.getParsingUIMessage();
-        Log.d("ONVIF",succeeded);
+    static class Action extends AsyncTask<String, Void, Void> {
 
-        if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetServices) {
+        @Override
+        protected Void doInBackground(String... strings) {
+            String req = strings[0];
 
-            currentDevice.getDeviceInformation();
-        } else if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetDeviceInformation) {
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .connectTimeout(10000, TimeUnit.SECONDS)
+                    .writeTimeout(100, TimeUnit.SECONDS)
+                    .readTimeout(10000, TimeUnit.SECONDS)
+                    .build();
 
-            currentDevice.getProfiles();
-        } else if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetProfiles) {
+            MediaType reqBodyType = MediaType.parse("application/soap+xml; charset=utf-8;");
 
-            currentDevice.getStreamURI();
-        } else if (onvifResponse.getRequest().getType() == OnvifRequest.Type.GetStreamURI) {
+            float x = 0;
+            float y = 0;
 
-            Log.d("ONVIF", "Stream URI retrieved: " + currentDevice.getRtspURI());
+            switch (req) {
+                case "up":
+                    y = 1;
+                    break;
+                case "down":
+                    y = -1;
+                    break;
+                case "left":
+                    x = -1;
+                    break;
+                default:
+                    x = 1;
+                    break;
+            }
+
+            RequestBody reqBody = RequestBody.create(reqBodyType,
+                    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                            "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\"\n" +
+                            " xmlns:tptz=\"http://www.onvif.org/ver20/ptz/wsdl\"\n" +
+                            " xmlns:tt=\"http://www.onvif.org/ver10/schema\">\n" +
+                            "  <soap:Body>\n" +
+                            "    <tptz:ContinuousMove>\n" +
+                            "      <tptz:ProfileToken>protoken_1</tptz:ProfileToken>\n" +
+                            "      <tptz:Velocity>\n" +
+                            "        <tt:PanTilt x=\"" + x + "\" y=\"" + y + "\"/>\n" +
+                            "        <tt:Zoom x=\"-1\"/>\n" +
+                            "      </tptz:Velocity>\n" +
+                            "    </tptz:ContinuousMove>\n" +
+                            "  </soap:Body>\n" +
+                            "</soap:Envelope>");
+
+            Request request = null;
+            request = new Request.Builder()
+                    .url("http://192.168.15.43:80/onvif/device_service")
+                    .addHeader("Content-Type", "text/xml; charset=utf-8")
+                    .addHeader("Authorization", "Digest username=\"student\", realm=\"onvif\", nonce=\"5c6c3321ca72737b8ddc\", uri=\"/onvif/device_service\", response=\"b65dce855d144627e01d487c2e1ec61c\", cnonce=\"a1b390a149f9085d64598b75f3a9e0f1\", nc=00000001, qop=\"auth,auth-int\"")
+                    .post(reqBody)
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                int i = 42;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
+
 }
