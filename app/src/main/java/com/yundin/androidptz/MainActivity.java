dda3056c3a066593ba74abc8588196b0be610f49
Vladislav Yundin <4yundin@gmail.com>
2019-02-21 23:34:09 +0300

    Force Http logging added

--- app/src/main/java/com/yundin/androidptz/MainActivity.java
+++ app/src/main/java/com/yundin/androidptz/MainActivity.java
@@ -2,16 +2,33 @@ package com.yundin.androidptz;
 
 import android.os.Bundle;
 
+import com.burgstaller.okhttp.AuthenticationCacheInterceptor;
+import com.burgstaller.okhttp.CachingAuthenticatorDecorator;
+import com.burgstaller.okhttp.digest.CachingAuthenticator;
+import com.burgstaller.okhttp.digest.Credentials;
+import com.burgstaller.okhttp.digest.DigestAuthenticator;
 import com.yundin.androidptz.model.ContinuousMoveRequest;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
 import androidx.appcompat.app.AppCompatActivity;
+import be.teletask.onvif.OnvifExecutor;
 import be.teletask.onvif.OnvifManager;
+import be.teletask.onvif.listeners.OnvifMediaProfilesListener;
+import be.teletask.onvif.listeners.OnvifMediaStreamURIListener;
 import be.teletask.onvif.listeners.OnvifResponseListener;
 import be.teletask.onvif.models.OnvifDevice;
+import be.teletask.onvif.models.OnvifMediaProfile;
 import be.teletask.onvif.responses.OnvifResponse;
 import io.github.controlwear.virtual.joystick.android.JoystickView;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -46,5 +63,49 @@ public class MainActivity extends AppCompatActivity {
                 onvifManager.sendOnvifRequest(device, new ContinuousMoveRequest(x, y, 0));
             }
         }, 500);
+
+        // force logging
+        Field f;
+        try {
+            f = onvifManager.getClass().getDeclaredField("executor");
+            f.setAccessible(true);
+            OnvifExecutor executor = (OnvifExecutor) f.get(onvifManager);
+
+            f = executor.getClass().getDeclaredField("credentials");
+            f.setAccessible(true);
+            Credentials credentials = (Credentials) f.get(executor);
+
+            DigestAuthenticator authenticator = new DigestAuthenticator(credentials);
+            Map<String, CachingAuthenticator> authCache = new ConcurrentHashMap<>();
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .connectTimeout(10000, TimeUnit.SECONDS)
+                    .writeTimeout(100, TimeUnit.SECONDS)
+                    .readTimeout(10000, TimeUnit.SECONDS)
+                    .authenticator(new CachingAuthenticatorDecorator(authenticator, authCache))
+                    .addInterceptor(new AuthenticationCacheInterceptor(authCache))
+                    .addInterceptor(logging)
+                    .build();
+
+            f = executor.getClass().getDeclaredField("client");
+            f.setAccessible(true);
+            f.set(executor, client);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        onvifManager.getMediaProfiles(device, new OnvifMediaProfilesListener() {
+            @Override
+            public void onMediaProfilesReceived(OnvifDevice device, List<OnvifMediaProfile> mediaProfiles) {
+                onvifManager.getMediaStreamURI(device, mediaProfiles.get(0), new OnvifMediaStreamURIListener() {
+                    @Override
+                    public void onMediaStreamURIReceived(OnvifDevice device, OnvifMediaProfile profile, String uri) {
+
+                    }
+                });
+            }
+        });
     }
 }
